ab433485bb0b99522ada6c382344cd06fe4eff33
korewkov <koreshkov2314@gmail.com>
2026-05-25 23:37:23 +0700

    Create supabase-config.js

---
+++ src/supabase-config.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  window.SUPABASE_CONFIG = {
+    url: 'https://mqswiynuuthwsrmsoajd.supabase.co',
+    anonKey: 'sb_publishable_8hLDSD8_-OfGxXYXdQy_4Q_3IzGPwYC'
+  };
+})();
